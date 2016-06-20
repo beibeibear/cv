eee1effd8f2f673adfd71cf8792b245d8d6cf268
djhrainbow <18829211547@163.com>
2016-06-20 16:24:22 +0800

    my

---
+++ cv.js
@@ -0,0 +1,8 @@
+$(function() {
+            $("#fullpage").fullpage(
+                {
+                    continuousVertical: true,  
+
+                    'navigation': true,
+                });
+        });
